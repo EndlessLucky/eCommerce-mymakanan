55677170582180b8658dd93e5d10237c5fdf2973
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2020-07-12 15:31:47 +1000

    register valid part

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm }   from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { ProductService } from '../../service/product.service';
 import { Http } from '@angular/http';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'mymakanan-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
@@ -26,10 +27,20 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(form: NgForm): void{
-    console.log(form.value);
     this.headers = new Headers({'Content-Type': 'application/json'});
-    this.http.post('http://127.0.0.1:8000/api/users', form.value).subscribe(res => {
-      console.log(res);
-    });
+    // if (form.value.address1 === '' || form.value.address2 === '' || form.value.area === '' || form.value.email === ''
+    //     || form.value.fullname === '' || form.value.identify === '' || form.value.password === '' || form.value.phonenumber === ''
+    //     || form.value.state === ''){
+    //   alert('Please input the field');
+    //   return;
+    // }
+    if (form.value.identify.length !== 12 || form.value.identify.substr(6,2) === '00') {
+      alert('Please input the correct identification number');
+      return;
+    }
+
+    // this.http.post('http://127.0.0.1:8000/api/users', form.value).subscribe(res => {
+    //   console.log(res);
+    // });
   }
 }
